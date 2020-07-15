5ad92e7407c21db077e16f6b80cdeb74c7d5e047
TulioliAles <desmecad@hotmail.com>
2020-07-14 21:14:44 -0300

    Commit 1 - Iniciado o projeto e criado a conexão com banco de dados MySQL

---
+++ src/main/java/br/com/conhecimento/BaseDeConhecimentoApplication.java
@@ -0,0 +1,13 @@
+package br.com.conhecimento;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BaseDeConhecimentoApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BaseDeConhecimentoApplication.class, args);
+	}
+
+}
